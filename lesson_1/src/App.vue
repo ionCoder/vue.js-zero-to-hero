12203ceb1a9d8163b444a1b521f17b438ed51d30
Simon Carr <simonjcarr@gmail.com>
2018-09-09 15:57:52 +0100

    lesson 1

---
+++ lesson_1/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div id="app">
+    <h1>Reactive Example</h1>
+    Enter your name: <input type="text" v-model="name">
+    <br>
+    Hello, {{name}} {{message}}
+
+
+    <br>
+    <button v-on:click="btnClick">Click me</button>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return {
+      name: '',
+      message: ''
+    }
+  },
+  methods:{
+    btnClick(){
+      this.message = "Vue is Awesome!!"
+    }
+  }
+}
+</script>
